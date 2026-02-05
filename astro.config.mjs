db3d137f6dbf1cdcfead9cb8998877117486b119
z-s-kusz <z.s.kusz@gmail.com>
2026-02-04 21:46:13 -0600

    add netlify adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
+  output: 'server',
+  // to run locally comment out adapter line -- I was getting tslib errors with it
+  adapter: netlify(),
 });
